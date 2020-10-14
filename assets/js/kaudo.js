8745a48b77059fb8fee88688015ff2347bf378a1
kAUdo <kaudo@msn.com>
2020-10-14 12:29:20 +0900

    LPC20

--- assets/js/kaudo.js
+++ assets/js/kaudo.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-  console.log('ㅇㄷ');
+$(window).resize(resizeWindow=function() {
   if($('#areaLeft:visible').length>0){
     $('#areaTop').addClass('hidden');
   }else{
     $('#areaTop').removeClass('hidden');
   }
+});
+
+$(document).ready(function(){
+  resizeWindow();
+
+  $('.page__content hr').each(function(idx,obj){
+    $('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:block;width:100%;min-height:100px;" data-ad-client="ca-pub-5585342941485872" data-ad-slot="7250574567" data-ad-format="auto" data-full-width-responsive="true"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>').insertBefore($(this));
+  });
+
 });
